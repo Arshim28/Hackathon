57b3e92528b0674d87ff0584e1e4400596cc1785
Arshim <98533618+Arshim28@users.noreply.github.com>
2023-01-15 14:34:46 +0530

    Added multiple pages

--- resume_scrape.py
+++ resume_scrape.py
@@ -1,49 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
 
-DEBUG=input("DEBUG?")
-if DEBUG=="no":
+DEBUG = input("DEBUG?")
+if DEBUG == "no":
     skills = input('Enter your Skill: ').strip().split()
     titles = input('Enter your Title:').strip().split()
     schools = input('Enter your School:').strip().split()
     places = input('Enter the location: ').strip().split()
 else:
-    skills=["coding"]
-    titles=["software", "developer"]
-    schools=[]
-    places=["india"]
+    skills = ["coding"]
+    titles = ["software", "developer"]
+    schools = []
+    places = ["india"]
 
-url="https://www.postjobfree.com/resumes?q="
+url = "https://www.postjobfree.com/resumes?q="
 for skill in skills:
-    url+=str(skill)+'+'
+    url += str(skill)+'+'
 for title in titles:
-    url+=str(title)+'+'
+    url += str(title)+'+'
 for school in schools:
-    url+=str(school)+'+'
-url=url[:-1]+'&l='
-if len(places)>0:
+    url += str(school)+'+'
+url = url[:-1]+'&l='
+if len(places) > 0:
     for place in places:
-        url+=str(place)+'%2C+'
-    url=url[:-3]
-url+="&radius=100"
+        url += str(place)+'%2C+'
+    url = url[:-3]
+url += "&radius=100"
 
-response=requests.get(url)
+#Resumes = []
 
-html=response.text
-soup=BeautifulSoup(html, "html.parser")
+for i in range(2, 10):
+    if i != 0:
+        url += f"&p={i}"
+    response = requests.get(url)
 
-resume_headers=soup.find_all("div", class_="snippetPadding")
-for resume_snippets in resume_headers:
-    resume_title=resume_snippets.find("h3", class_="itemTitle")
-    resume_link=resume_title.find("a")
-    print(resume_link.text)
-    
-    #print(resume_title)
-#print(resume_headers)
-#print(soup.prettify())
-#a=soup.prettify()
-#f=open("C:/Users/aarya/Downloads/resume_output.txt", "w")
-#f.write(a)
-#f.close()
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
 
-#https://www.postjobfree.com/resumes?q=coding+software+developer+python+java&l=Mumbai%2C+Maharashtra%2C+India&radius=25
+    resume_headers = soup.find_all("div", class_="snippetPadding")
+
+    for resume_snippets in resume_headers:
+        resume_title = resume_snippets.find("h3", class_="itemTitle")
+        resume_link = resume_title.find("a")
+        print(resume_link.text)
